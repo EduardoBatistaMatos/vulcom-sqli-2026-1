b0122468d15eaa71dd0599f1162de25e1df25d39
Eduardo Batista Matos <162808278+EduardoBatistaMatos@users.noreply.github.com>
2026-03-28 13:52:29 +0000

    (26/03) Exploração da vulnerabilidade SQL Injection

--- index.js
+++ index.js
@@ -27,10 +27,11 @@ app.get('/', (req, res) => {
 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
-    
+
     // CONSULTA SQL VULNERÁVEL 🚨
-    const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
-    
+    const query = "SELECT * FROM users WHERE username = '?' AND password = '? '";
+
+
     db.all(query, [], (err, rows) => {
         if (err) {
             return res.send('Erro no servidor');
